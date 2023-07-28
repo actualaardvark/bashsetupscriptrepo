844b2b4bba4cb61fa7a24035bddc69ae7f8ebe9a
actualaardvark <actualaardvark@users.noreply.github.com>
2023-07-28 14:08:57 -0700

    Add mirror of readme.md installer

---
+++ installtousb.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# A mirror of the install script from README.md
+
+curl "https://raw.githubusercontent.com/actualaardvark/bashsetupscriptrepo/main/updateandrun" > /volumes/"$(df -h | grep -v "/System" | grep "/Volumes" | awk -F'/Volumes/' '{print $2}')"/updateandrun && chmod +x /volumes/"$(df -h | grep -v "/System" | grep "/Volumes" | awk -F'/Volumes/' '{print $2}')"/updateandrun && curl "https://raw.githubusercontent.com/actualaardvark/bashsetupscriptrepo/main/update.sh" > /volumes/"$(df -h | grep -v "/System" | grep "/Volumes" | awk -F'/Volumes/' '{print $2}')"/update.sh && chmod +x /volumes/"$(df -h | grep -v "/System" | grep "/Volumes" | awk -F'/Volumes/' '{print $2}')"/update.sh && curl "https://raw.githubusercontent.com/actualaardvark/bashsetupscriptrepo/main/values.conf" > /volumes/"$(df -h | grep -v "/System" | grep "/Volumes" | awk -F'/Volumes/' '{print $2}')"/values.conf && chmod +x /volumes/"$(df -h | grep -v "/System" | grep "/Volumes" | awk -F'/Volumes/' '{print $2}')"/values.conf
